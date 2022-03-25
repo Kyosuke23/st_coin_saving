5ff62a0351f718361ac98589843e14b76a21d0c1
Kyosuke <k.ikeda@aiosl-tec.co.jp>
2022-03-25 12:45:40 +0900

    検索APIのURIを修正

--- app.py
+++ app.py
@@ -6,17 +6,25 @@ import ast
 
 TODAY = datetime.datetime.today().strftime('%Y-%m-%d')
 URL_INDEX = 'https://qjljun.deta.dev/'
-URL_GET_ALL_DATA = 'https://qjljun.deta.dev/datas'
-URL_GET_DATA_BY_DATE = 'https://qjljun.deta.dev/datas/' + TODAY
+URL_GET_ALL_DATA = 'https://qjljun.deta.dev/data'
+URL_GET_DATA_BY_DATE = 'https://qjljun.deta.dev/data/' + TODAY
 
-# 今日付のデータを取得
-response = requests.get(URL_GET_DATA_BY_DATE)
+# サイドバーで選択されたページを取得
+page = st.sidebar.selectbox('Choose your page', ['main', 'log'])
 
-# 取得したデータを変換(=> dict)
-today_data = ast.literal_eval(json.loads(response.text))
+# メイン画面
+if page == 'main':
+    # 今日付のデータを取得
+    response = requests.get(URL_GET_DATA_BY_DATE)
+    # 取得したデータを変換(=> dict)
+    today_data = ast.literal_eval(json.loads(response.text))
 
-# 画面項目の描画
-st.title('500円玉貯金アプリ')
-st.write((f'日付: {today_data["SAVING_DATE"]}'))
-st.write((f'今日の貯金枚数: {today_data["AMOUNT"]}枚'))
-st.write((f'今までの貯金額: ¥{today_data["TOTAL_AMOUNT"] * 500:,}'))
+    # 画面項目の描画
+    st.title('500円玉貯金アプリ')
+    st.write((f'日付: {today_data["SAVING_DATE"]}'))
+    st.write((f'今日の貯金枚数: {today_data["AMOUNT"]}枚'))
+    st.write((f'今までの貯金額: ¥{today_data["TOTAL_AMOUNT"] * 500:,}'))
+
+elif page == 'log':
+    # 全データを取得
+    response = requests.get(URL_GET_ALL_DATA)
