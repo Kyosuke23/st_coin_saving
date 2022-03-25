52b225756bfbe4b83255e3a6f0198a46fc3c6963
Kyosuke <k.ikeda@aiosl-tec.co.jp>
2022-03-25 14:58:18 +0900

    ログページを実装

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import requests
 from datetime import datetime, timedelta
 import json
 import ast
+import pandas as pd
+import numpy as np
 
 # 今日の日付
 TODAY = datetime.today()
@@ -20,7 +22,7 @@ URL_GET_DATA_BY_DATE = URL_GET_ALL_DATA + TODAY.strftime('%Y-%m-%d')
 page = st.sidebar.selectbox('Choose your page', ['main', 'log'])
 
 # メイン画面
-if page == 'main':
+if page == 'main1':
     # 今日付のデータを取得
     response = requests.get(URL_GET_DATA_BY_DATE)
     # 取得したデータを変換(=> dict)
@@ -32,6 +34,25 @@ if page == 'main':
     st.write((f'今日の貯金枚数: {today_data["AMOUNT"]}枚'))
     st.write((f'今までの貯金額: ¥{today_data["TOTAL_AMOUNT"] * 500:,}'))
 
-elif page == 'log':
-    # 全データを取得
-    response = requests.get(URL_GET_ALL_DATA)
+elif page == 'main':
+    # 1年前の日付
+    DATE_PASR_1Y = TODAY + timedelta(days=-365)
+
+    # 範囲検索のURI生成
+    URL_GET_DATA_BETWEEN_DATE = URL_GET_ALL_DATA + DATE_PASR_1Y.strftime('%Y-%m-%d') + '/' + TODAY.strftime('%Y-%m-%d')
+    
+    # 過去1年のデータを取得
+    response = requests.get(URL_GET_DATA_BETWEEN_DATE)
+
+    # 取得したデータを変換(=> dict)
+    log_date = ast.literal_eval(json.loads(response.text))
+
+    # 画面項目の描画
+    st.title('500円玉貯金ログ')
+
+    # # データフレームの描画
+    df = pd.DataFrame(log_date) # データフレーム化
+    df = df.sort_values('SAVING_DATE', ascending=False) # 降順にソート
+    df['TOTAL_PRICE'] = df['TOTAL_AMOUNT'] * 500 # 累計金額列を追加
+    df = df[['SAVING_DATE', 'AMOUNT', 'TOTAL_AMOUNT', 'TOTAL_PRICE', 'UPDATED_AT']] # 表示列を定義
+    st.dataframe(df, 800, 800)
